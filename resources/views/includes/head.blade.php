bf51db4551ca38ef7c03fc732bf24b382f056683
varanustoken <varanustoken@gmail.com>
2023-04-30 13:32:26 +0700

    a

--- resources/views/includes/head.blade.php
+++ resources/views/includes/head.blade.php
@@ -3,36 +3,22 @@
 <meta http-equiv="Content-Type" content="text/html; charset=UTF-8">
 <meta http-equiv="X-UA-Compatible" content="IE=edge">
 
-<meta name="description" content="AI ERUDEX | MACHINE LEARNING">
-<meta name="keywords" content="ai, erudex, smartchain, machinelearning, blockchain, cryptocurrency, bsc">
-<meta name="author" content="ERUDEX">
+<meta name="description" content="PEPE GOKU">
+<meta name="keywords" content="pepe, goku, pepegoku, ethereum, erc20">
+<meta name="author" content="PEPEGOKU">
 
-<title>AI ERUDEX | MACHINE LEARNING</title>
+<title>PEPE GOKU</title>
 <link rel="icon" type="image/x-icon" href="{{ asset('images/favicon.png') }}" sizes="16x16">
 
-<!-- Fonts -->
-<link href="https://fonts.googleapis.com/css?family=Comfortaa:300,400,500,700" rel="stylesheet">
-
-<link href="{{ asset('css/font-awesome.min.css') }}" rel="stylesheet">
-
-<!-- bootstrap css -->
-<link rel="stylesheet" href="{{ asset('css/bootstrap.min.css') }}" type="text/css" media="all" />
-<!-- animate css -->
-<link rel="stylesheet" href="{{ asset('css/animate.min.css') }}" type="text/css" media="all" />
-<!-- owl carousel css -->
-<link rel="stylesheet" href="{{ asset('css/owl.carousel.min.css') }}"  type="text/css" media="all" />
-<link rel="stylesheet" href="{{ asset('css/owl.theme.default.min.css') }}"  type="text/css" media="all" />
-<!-- meanmenu css -->
-<link rel="stylesheet" href="{{ asset('css/meanmenu.min.css') }}" type="text/css" media="all" />
-<!-- magnific popup css -->
-<link rel="stylesheet" href="{{ asset('css/magnific-popup.min.css') }}" type="text/css" media="all" />
-<!-- boxicons css -->
-<link rel='stylesheet' href="{{ asset('css/icofont.min.css') }}" type="text/css" media="all" />
-<!-- flaticon css -->
-<link rel='stylesheet' href="{{ asset('css/flaticon.css') }}" type="text/css" media="all" />
-<!-- nice select css -->
-<link rel="stylesheet" href="{{ asset('css/nice-select.css') }}" type="text/css" media="all" />
-<!-- style css -->
-<link rel="stylesheet" href="{{ asset('css/style.css') }}" type="text/css" media="all" />
-<!-- responsive css -->
-<link rel="stylesheet" href="{{ asset('css/responsive.css') }}" type="text/css" media="all" />
+<!-- Bootstrap CSS -->
+<link rel="stylesheet" href="{{ asset('css/bootstrap.min.css') }}">
+<!-- Fontawesome CSS -->
+<link rel="stylesheet" href="{{ asset('css/fontawesome-all.min.css') }}">
+<!-- Vegas CSS -->
+<link rel="stylesheet" href="{{ asset('vendor/vegas/vegas.min.css') }}">
+<!-- Custom Animation CSS -->
+<link rel="stylesheet" href="{{ asset('css/fxt-animation.css') }}">
+<!-- Custom CSS -->
+<link rel="stylesheet" href="{{ asset('style.css') }}">
+<!-- Google Font -->
+<link href="https://fonts.googleapis.com/css2?family=Poppins:wght@300;400;500;600;700&amp;display=swap" rel="stylesheet">
